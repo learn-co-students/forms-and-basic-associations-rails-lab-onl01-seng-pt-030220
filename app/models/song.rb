ab9742b1020e9a83619fabff72a030b298361b38
alelee93 <leevargasale@gmail.com>
2020-07-20 18:29:03 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,18 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes 
+
+  def artist_name=(name)
+  end
+
+  def artist_name
+  end
+
+  def note_ids=(ids)
+    ids.each do |id|
+      note = Note.find(id)
+      self.notes << note
+    end
+  end
 end
