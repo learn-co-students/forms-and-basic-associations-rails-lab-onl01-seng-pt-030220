fd66ea306bf03477aa2622d87ba8414aab50770b
cdominick091984 <cdominick091984@gmail.com>
2020-07-25 20:39:02 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -19,30 +19,15 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def note_contents=(notes)
-    notes.each do |note|
-      self.notes.build(content: note).save if note.length > 0
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.strip != ""
+         self.notes.build(content: content)
+      end
     end
   end
 
   def note_contents
-    self.notes.map do |note|
-      note.content
-    end
+    self.notes.map(&:content)
   end
-
-  def note_ids=(ids)
-    ids.each do |id|
-      id_num = id.chars.last.to_i
-      note = Note.find_or_create_by(id: id_num)
-      self.notes << note
-    end
-  end
-
-  def note_ids
-    self.notes.map do |note|
-      "song_notes_#{note.id}"
-    end
-  end
-
 end
